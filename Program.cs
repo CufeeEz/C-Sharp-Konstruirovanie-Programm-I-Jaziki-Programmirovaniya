8dfb6b6f648c7bcb0c0b5ed245e3cbc7f429cc61
Daniil <92633278+CufeeEz@users.noreply.github.com>
2022-09-29 08:43:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывести пользователю простое сообщение.
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Кастевич Даниил Алексеевич, *номер зачетки*, Конструирование программ и языки программирования, 1 лабараторная работа.");
+            //Ожидать нажатия клавиши <Enter> перед завершением работы.
+            Console.ReadLine();
+        }
+    }
+}
