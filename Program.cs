aa48d8ddad5415edc43c3ab9c6371e9a459e4e52
Daniil <92633278+CufeeEz@users.noreply.github.com>
2022-09-29 08:44:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.234;
+            int b = 1;
+            double x = 3.413;
+            double y = 1.789;
+            double e = 2.71828;
+            double z;
+            z = (x + b) / (y + (b * b)) + (Math.Log(a) * (x + y)) / (Math.Pow(e, x) + y);
+            Console.WriteLine($"Ответ: {z}");
+        }
+    }
+}
