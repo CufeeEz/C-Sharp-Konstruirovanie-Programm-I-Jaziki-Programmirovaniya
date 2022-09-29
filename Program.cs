510b9443fa8798406ab998a026e34bac51b91a2b
Daniil <92633278+CufeeEz@users.noreply.github.com>
2022-09-29 08:43:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0.97;
+            double y = -0.0024;
+            double J;
+            double e = 2.71828;
+            double p;
+            J = Math.Pow(e, (4 * x / 5) + 2 * Math.Sin(7 * Math.Log(x + (5 / 3))));
+            p = J;
+            x = -0.5;
+            Console.WriteLine($"J1 = {J}");
+            J = Math.Pow(e, (4 * x / 5) + 2 * Math.Sin(7 * Math.Log(x + (5 / 3))-p));
+            Console.WriteLine($"J2 = {J}");
+        }
+    }
+}
